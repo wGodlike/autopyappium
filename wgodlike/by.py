813191b1509d2cecc44ea039debd3a087ee1b1f5
wangqingbin <714227638@qq.com>
2019-09-08 21:50:34 +0800

    common

--- wgodlike/by.py
+++ wgodlike/by.py
@@ -12,17 +12,40 @@ def get_locator(key):
     print(res)
     by_type = res.get(key).split('>')[0]
     by_val = res.get(key).split('>')[1]
-    if by_type == 'id':
-        print(by_val)
-        return
-    elif by_type == 'xpath':
-        print(by_val)
+    if by_type is 'id':
+        el = (By.ID, by_val)  # drive.find_element(el).click()
+        return el
+    elif by_type is 'xpath':
+        el = (By.XPATH, by_val)
+        return el
     else:
         print('输入类型无效')
 
 
+"""
+# 导入By
+from selenium.webdriver.common.by import By
+# id定位
+find_element(By.ID, "id值")
+# name属性定位
+find_element(By.NAME, "name值")
+# class属性定位
+find_element(By.CLASS_NAME, "class值")
+# 标签定位
+find_element(By.TAG_NAME, "input")
+#定位链接
+find_element(By.LINK_TEXT, "新闻")
+# 定位长链接
+find_element(By.PARTIAL_LINK_TEXT, "新")
+# xpath定位
+find_element(By.XPATH, "//*[@class='值']")
+# css定位
+find_element(By.CSS_SELECTOR, "span.class值>input#值")
+"""
+
 if __name__ == '__main__':
-    s = '123'
+    s = 'ABCD'
     s.split()
-    print(get_locator('gobtn'))
+    print(s is 'ABCD')
+    # print(get_locator('gobtn'))
 
--- wgodlike/common.py
+++ wgodlike/common.py
@@ -2,8 +2,71 @@
 # -*- coding: utf-8 -*-
 # Created by wGodlike at 2019-09-08
 # @Desc
+import time
+
+from selenium.webdriver.support.wait import WebDriverWait
+
+from wgodlike import logger
 from wgodlike.base import Base
 
 
 class Common(Base):
-    pass
+
+    def __init__(self, driver) -> None:
+        # self.driver = driver
+        self.log = logger.Logger()
+        super().__init__(driver)
+
+    def get_size(self):
+        x = self.driver.get_window_size()['width']
+        y = self.driver.get_window_size()['height']
+        return x, y
+
+    def switch(self, name):
+        # 'WEBVIEW_com.wondershare.drfone'
+        # 'NATIVE_APP'
+        contexts = self.driver.contexts
+        print(contexts)
+        self.driver.switch_to.context(name)
+
+    def get_toast(self):
+        limit_message = "用户名或密码错误，你还可以尝试3次"
+        message = '//*[@text=\'{}\']'.format(limit_message)
+        toast_element = WebDriverWait(self.driver, 5).until(lambda x: x.find_element_by_xpath(message))
+        print(toast_element.text)
+        return toast_element.text
+
+    def get_images(self, name):
+        name += '_' + str(int(time.time()))
+        self.driver.save_screenshot('logs/images/' + name + '.png')
+        print(name)
+
+    def swipe_to(self, direction):
+        """
+        :param direction: 指明滑动方向
+        :return:
+        """
+        w = self.get_size()
+        if direction is 'left':
+            x1 = int(w[0] * 0.9)
+            y1 = int(w[1] * 0.5)
+            x2 = int(w[0] * 0.1)
+            self.driver.swipe(x1, y1, x2, y1, 1500)
+        elif direction is 'up':
+            x1 = int(w[0] * 0.5)
+            y1 = int(w[1] * 0.95)
+            y2 = int(w[1] * 0.35)
+            self.driver.swipe(x1, y1, x1, y2, 1000)
+        elif direction is 'right':
+            pass
+        elif direction is 'up':
+            pass
+        else:
+            self.log.logger.error('数据错误, 只支持 left、up、right、down')
+
+
+if __name__ == '__main__':
+    s = 'abc'
+    print(s)
+    s += '_' + str(int(time.time()))
+    print(s)
